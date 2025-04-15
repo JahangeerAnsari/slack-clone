49c63ef648e0ba9ddbf6f440dc02a7fdea50b9e1
Jahangeer Ansari <jahangeeransari194@gmail.com>
2025-04-15 10:08:37 +0530

    remove unnecessary code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,7 @@
 "use client";
-import { Button } from "@/components/ui/button";
-// import AuthScreen from "@/features/auth/components/auth-screen";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { useAuthActions } from "@convex-dev/auth/react";
+
 import UserButton from "@/features/auth/components/user-button";
 
-
-
 export default function Home() {
-  const {signOut} = useAuthActions()
-  return (
-    // <AuthScreen/>
-    <div>
-      <div>LOGINNNNN SCREENM</div>
-      {/* <Button onClick={() => signOut()}>Logout</Button> */}
-      <UserButton></UserButton>
-      
-    </div>
-  );
+  return <UserButton />;
 }
